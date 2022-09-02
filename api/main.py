51aa81c05db22fc4776a0fce55a93c1b22535b86
blinkspark <3829294+blinkspark@users.noreply.github.com>
2022-09-02 16:41:10 +0800

    add verify

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from ast import Pass
 from ctypes.wintypes import ULONG
+from enum import Flag
 from typing import Tuple
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -13,6 +14,10 @@ from datetime import datetime, timedelta
 class UserRequest(BaseModel):
   uname: str
   passwd: str
+
+
+class VerifyRequest(BaseModel):
+  token: str
 
 
 app = FastAPI()
@@ -55,6 +60,24 @@ async def login(user: UserRequest):
     except VerifyMismatchError as e:
       return {'ok': False, 'error': 'passowrd does not match'}
     except Exception as e:
-      return {'ok': False, 'error': 'unknown error'}
+      return {'ok': False, 'error': f'unknown error {e}'}
 
   return {'ok': False, 'error': 'user not found'}
+
+
+@app.post("/verify")
+async def verify(req: VerifyRequest):
+  header = jwt.get_unverified_header(req.token)
+  col = db.collection('users')
+  uname = header['uname']
+  async for user in col.where('username', '==', uname).stream():
+    user = user.to_dict()
+    key = user['key']
+    try:
+      _ = jwt.decode(req.token, key=key, algorithms=['HS256'])
+      return {'ok': True}
+    except Exception as e:
+      return {'ok': False, 'error': f'unknown error {e}'}
+    finally:
+      break
+  return {'ok': False, 'error': 'unknown error'}
